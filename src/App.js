917825a84dda0202900b56b7de5db0bec083e79d
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-07-30 20:49:11 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import Pokedex from "./Pokedex"
-import Pokecard from "./Pokecard";
 import Pokegame from "./Pokegame";
 
 function App() {
@@ -13,3 +11,20 @@ function App() {
 }
 
 export default App;
+
+
+// import React from "react";
+
+// import Pokedex from "./Pokedex"
+// import Pokecard from "./Pokecard";
+// import Pokegame from "./Pokegame";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Pokegame />
+//     </div>
+//   );
+// }
+
+// export default App;
